fd1dda7031d3830b427edb39f18ac91145a2fdb0
Vinicius Niche <viniciusnichecorrea@gmail.com>
2022-03-12 14:07:21 -0300

    release twitter v0.0.6

---
+++ twitter/v1/metrics_pb.d.ts
@@ -0,0 +1,118 @@
+// package: io.inpublic.twitter.v1
+// file: twitter/v1/metrics.proto
+
+import * as jspb from "google-protobuf";
+
+export class IntervalAggregateQuery extends jspb.Message {
+  getRange(): string;
+  setRange(value: string): void;
+
+  getInterval(): string;
+  setInterval(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): IntervalAggregateQuery.AsObject;
+  static toObject(includeInstance: boolean, msg: IntervalAggregateQuery): IntervalAggregateQuery.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: IntervalAggregateQuery, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): IntervalAggregateQuery;
+  static deserializeBinaryFromReader(message: IntervalAggregateQuery, reader: jspb.BinaryReader): IntervalAggregateQuery;
+}
+
+export namespace IntervalAggregateQuery {
+  export type AsObject = {
+    range: string,
+    interval: string,
+  }
+}
+
+export class ContributionMetrics extends jspb.Message {
+  getValuesMap(): jspb.Map<string, number>;
+  clearValuesMap(): void;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ContributionMetrics.AsObject;
+  static toObject(includeInstance: boolean, msg: ContributionMetrics): ContributionMetrics.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ContributionMetrics, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ContributionMetrics;
+  static deserializeBinaryFromReader(message: ContributionMetrics, reader: jspb.BinaryReader): ContributionMetrics;
+}
+
+export namespace ContributionMetrics {
+  export type AsObject = {
+    valuesMap: Array<[string, number]>,
+  }
+}
+
+export class RangeAggregateQuery extends jspb.Message {
+  getRange(): string;
+  setRange(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): RangeAggregateQuery.AsObject;
+  static toObject(includeInstance: boolean, msg: RangeAggregateQuery): RangeAggregateQuery.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: RangeAggregateQuery, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): RangeAggregateQuery;
+  static deserializeBinaryFromReader(message: RangeAggregateQuery, reader: jspb.BinaryReader): RangeAggregateQuery;
+}
+
+export namespace RangeAggregateQuery {
+  export type AsObject = {
+    range: string,
+  }
+}
+
+export class ReachMetrics extends jspb.Message {
+  clearValuesList(): void;
+  getValuesList(): Array<number>;
+  setValuesList(value: Array<number>): void;
+  addValues(value: number, index?: number): number;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ReachMetrics.AsObject;
+  static toObject(includeInstance: boolean, msg: ReachMetrics): ReachMetrics.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ReachMetrics, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ReachMetrics;
+  static deserializeBinaryFromReader(message: ReachMetrics, reader: jspb.BinaryReader): ReachMetrics;
+}
+
+export namespace ReachMetrics {
+  export type AsObject = {
+    valuesList: Array<number>,
+  }
+}
+
+export class StatsMetrics extends jspb.Message {
+  getAverage(): number;
+  setAverage(value: number): void;
+
+  getTotal(): number;
+  setTotal(value: number): void;
+
+  getSum(): number;
+  setSum(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): StatsMetrics.AsObject;
+  static toObject(includeInstance: boolean, msg: StatsMetrics): StatsMetrics.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: StatsMetrics, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): StatsMetrics;
+  static deserializeBinaryFromReader(message: StatsMetrics, reader: jspb.BinaryReader): StatsMetrics;
+}
+
+export namespace StatsMetrics {
+  export type AsObject = {
+    average: number,
+    total: number,
+    sum: number,
+  }
+}
+
---
+++ twitter/v1/metrics_pb_service.js
@@ -0,0 +1,181 @@
+// package: io.inpublic.twitter.v1
+// file: twitter/v1/metrics.proto
+
+var twitter_v1_metrics_pb = require("../../twitter/v1/metrics_pb");
+var grpc = require("@improbable-eng/grpc-web").grpc;
+
+var Metrics = (function () {
+  function Metrics() {}
+  Metrics.serviceName = "io.inpublic.twitter.v1.Metrics";
+  return Metrics;
+}());
+
+Metrics.ContributionsPerInterval = {
+  methodName: "ContributionsPerInterval",
+  service: Metrics,
+  requestStream: false,
+  responseStream: false,
+  requestType: twitter_v1_metrics_pb.IntervalAggregateQuery,
+  responseType: twitter_v1_metrics_pb.ContributionMetrics
+};
+
+Metrics.ContributionsStats = {
+  methodName: "ContributionsStats",
+  service: Metrics,
+  requestStream: false,
+  responseStream: false,
+  requestType: twitter_v1_metrics_pb.RangeAggregateQuery,
+  responseType: twitter_v1_metrics_pb.StatsMetrics
+};
+
+Metrics.ContributorsReach = {
+  methodName: "ContributorsReach",
+  service: Metrics,
+  requestStream: false,
+  responseStream: false,
+  requestType: twitter_v1_metrics_pb.RangeAggregateQuery,
+  responseType: twitter_v1_metrics_pb.ReachMetrics
+};
+
+Metrics.ContributorsStats = {
+  methodName: "ContributorsStats",
+  service: Metrics,
+  requestStream: false,
+  responseStream: false,
+  requestType: twitter_v1_metrics_pb.RangeAggregateQuery,
+  responseType: twitter_v1_metrics_pb.StatsMetrics
+};
+
+exports.Metrics = Metrics;
+
+function MetricsClient(serviceHost, options) {
+  this.serviceHost = serviceHost;
+  this.options = options || {};
+}
+
+MetricsClient.prototype.contributionsPerInterval = function contributionsPerInterval(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Metrics.ContributionsPerInterval, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+MetricsClient.prototype.contributionsStats = function contributionsStats(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Metrics.ContributionsStats, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+MetricsClient.prototype.contributorsReach = function contributorsReach(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Metrics.ContributorsReach, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+MetricsClient.prototype.contributorsStats = function contributorsStats(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Metrics.ContributorsStats, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+exports.MetricsClient = MetricsClient;
+
